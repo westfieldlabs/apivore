799ed16720b05d22127fa410ac99f49eaf153fe1
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-04-29 15:47:33 +1200

    Fix last integration test

--- spec/data/example_specs.rb
+++ spec/data/example_specs.rb
@@ -3,27 +3,6 @@ require 'spec_helper'
 context "API testing scenarios" do
   include Apivore::RspecMatchers
   include Apivore::RspecHelpers
-  # before do
-  #   apivore_setup '/services/{id}.json', 'get', '200' do
-  #     {'id' => 1}
-  #   end
-  #
-  #   apivore_setup '/services.json', 'post', '204' do
-  #     {"_data" => {'name' => 'hello world'}}
-  #   end
-  #
-  #   apivore_setup '/services/{id}.json', 'put', '204' do
-  #     {'id' => 1}
-  #   end
-  #
-  #   apivore_setup '/services/{id}.json', 'delete', '204' do
-  #     {'id' => 1}
-  #   end
-  #
-  #   apivore_setup '/services/{id}.json', 'patch', '204' do
-  #     {'id' => 1}
-  #   end
-  # end
 
   describe "undocumented path in test", :type => :request do
     subject { Apivore::SwaggerChecker.instance_for("/02_unimplemented_path.json") }
@@ -70,8 +49,16 @@ context "API testing scenarios" do
       expect(subject).to document(:get, "/services/{id}.json", 200, { "id" => 1})
     end
   end
-  #
-  # describe "missing non-required", :type => :request do
-  #   validate("/07_missing_non-required_property.json")
-  # end
+
+  describe "missing non-required", :type => :request do
+    subject { Apivore::SwaggerChecker.instance_for("/07_missing_non-required_property.json") }
+
+    it "fails" do
+      expect(subject).to document(:get, "/services.json", 200)
+    end
+
+    it "also fails" do
+      expect(subject).to document(:get, "/services/{id}.json", 200, { "id" => 1})
+    end
+  end
 end
