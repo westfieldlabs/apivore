ccab7a04d6dd42ec77090241c547ad4faf0e9761
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-05-13 09:59:08 -0600

    Fix testing the same path multiple times

--- lib/apivore/swagger_checker.rb
+++ lib/apivore/swagger_checker.rb
@@ -25,7 +25,8 @@ module Apivore
     end
 
     def fragment(path, method, code)
-      mappings[path][method.to_s][code.to_s]
+      path_fragment = mappings[path][method.to_s][code.to_s]
+      path_fragment.dup unless path_fragment.nil?
     end
 
     def remove_tested_end_point_response(path, method, code)
